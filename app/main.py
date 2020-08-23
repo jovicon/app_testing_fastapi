381308cfc2354af40c5107de9a41caa47d4fb8da
JoseContreras <jocontrera16@gmail.com>
2020-08-23 17:57:20 -0400

    uncomment directory

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 # from sqlalchemy.orm import Session
 
-# from .api import pokemons
+from .api import pokemons
 
 app = FastAPI()
 
-# app.include_router(pokemons.router, tags=["pokemons"])
+app.include_router(pokemons.router, tags=["pokemons"])
 
 # testing fast api
 @app.post("/hello_world")
